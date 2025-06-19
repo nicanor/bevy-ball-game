236789a0b91a14e3e9a9a3ed9cbd0271737d7304
Nicanor Perera <nicanorperera@gmail.com>
2025-06-18 21:59:38 -0300

    Many systems defined in the same Update

--- src/app_state.rs
+++ src/app_state.rs
@@ -5,12 +5,15 @@ pub struct AppStatePlugin;
 
 impl Plugin for AppStatePlugin {
     fn build(&self, app: &mut App) {
-        app.init_state::<AppState>()
-            .add_event::<GameOver>()
-            .add_systems(Update, exit_event)
-            .add_systems(Update, handle_game_over)
-            .add_systems(Update, transition_to_game_state)
-            .add_systems(Update, transition_to_main_menu_state);
+        app.init_state::<AppState>().add_event::<GameOver>().add_systems(
+            Update,
+            (
+                exit_event,
+                handle_game_over,
+                transition_to_game_state,
+                transition_to_main_menu_state,
+            ),
+        );
     }
 }
 
